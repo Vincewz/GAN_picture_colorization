80d110f41f144b55d35d75c4840830780b046ea4
Vincewz <vincent.wielgosz@gmail.com>
2024-12-05 12:11:28 +0100

    correction

--- network.py
+++ network.py
@@ -79,19 +79,3 @@ class CNNDiscriminator(tf.keras.Model):
     def call(self, x):
         return self.model(x)
 
-# ops.py
-def generator_loss(disc_generated_output, gen_output, target):
-    adversarial_loss = tf.keras.losses.BinaryCrossentropy()(
-        tf.ones_like(disc_generated_output), disc_generated_output)
-    l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
-    return adversarial_loss + 100 * l1_loss
-
-def discriminator_loss(disc_real_output, disc_generated_output):
-    real_loss = tf.keras.losses.BinaryCrossentropy()(
-        tf.ones_like(disc_real_output), disc_real_output)
-    fake_loss = tf.keras.losses.BinaryCrossentropy()(
-        tf.zeros_like(disc_generated_output), disc_generated_output)
-    return (real_loss + fake_loss) / 2
-
-def rgb_to_grayscale(image):
-    return tf.image.rgb_to_grayscale(image)
